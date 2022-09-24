be97faaaf22c37e9a12dbfdf5dd8ce8c82aeb6ea
VitorKruel102 <vkruel.programador@gmail.com>
2022-09-24 14:52:05 -0300

    Adicionado Valores e Tipos.

--- PensePython_Exercicios.py
+++ PensePython_Exercicios.py
@@ -10,12 +10,22 @@ def Operadores():
     subtracao = 43 - 1
     multiplicacao = 6 * 7
     divisao = 84 / 2
+    exponenciacao = 6 ** 2
 
     print(f'Soma: {soma}')
     print(f'Subtração: {subtracao}')
     print(f'Multiplicacao: {multiplicacao}')
     print(f'Divisão: {divisao}')
+    print(f'Exponenciação: {exponenciacao}')
     
 
+# 1.5 Valores e Tipos:
+def ValorTipos():
+    print(type(2))
+    print(type(42.0))
+    print(type('Hello, World!'))
+    pass
+
+
 if __name__ == '__main__':
-    Operadores()
+    ValorTipos()
