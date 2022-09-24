97969eb1415d278bacb7154d36414409f53339c9
VitorKruel102 <vkruel.programador@gmail.com>
2022-09-24 13:39:30 -0300

    Adicionado def Operadores Aritméticos.

--- PensePython_Exercicios.py
+++ PensePython_Exercicios.py
@@ -1,7 +1,25 @@
 # 1.3 Primeiro Programa: 
+from audioop import mul
+from pickletools import read_uint1
+from re import sub
+
+
 def PrimeiroPrograma():
-    print('Hello World')
+    print('Hello World!')
+
+
+# 1.4 Operadores Aritméticos:
+def Operadores():
+    soma = 40 + 2
+    subtracao = 43 - 1
+    multiplicacao = 6 * 7
+    divisao = 84 / 2
+
+    print(f'Soma: {soma}')
+    print(f'Subtração: {subtracao}')
+    print(f'Multiplicacao: {multiplicacao}')
+    print(f'Divisão: {divisao}')
     
 
 if __name__ == '__main__':
-    PrimeiroPrograma()
+    Operadores()
